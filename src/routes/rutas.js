43fb339eab7689000facc7fc6d74f5fde0f2b2e7
Alonso Molina <alonso.19.2005@gmail.com>
2024-10-31 19:57:43 -0600

    se mejoro el diseño

--- src/routes/rutas.js
+++ src/routes/rutas.js
@@ -4,6 +4,6 @@ const ruta = Router();
 const path = require('path');
 
 ruta.get('/', (req,res) => {res.sendFile(path.join(__dirname,'../vistas/cal.html'));});
-ruta.get('/Historial', (req,res) => {res.sendFile(path.join(__dirname,'../vistas/.html'));});
+ruta.get('/Historial', (req,res) => {res.sendFile(path.join(__dirname,'../vistas/historial.html'));});
 
 module.exports = ruta;
